6ec16732ad7e321b40d6766e2c1d5fa9205d8b56
Daniel Avila Avila <avila.daniel86@gmail.com>
2021-07-23 09:42:59 -0500

    Add project files.

---
+++ HttpContextAccessor/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using HttpContextAccessor.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpContextAccessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IRequestService _userService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IRequestService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _userService.GetRequestPath();
+        }
+    }
+}
---
+++ HttpContextAccessor/Services/RequestService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpContextAccessor.Services
+{
+    public class RequestService : IRequestService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RequestService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// This method is used to get access to the HttpContext from service class
+        /// </summary>
+        /// <returns></returns>
+        public string GetRequestPath()
+        {
+            return $"User requested {_httpContextAccessor.HttpContext.Request.Method} from {_httpContextAccessor.HttpContext.Request.Path.Value} path" ;
+        }
+    }
+}
